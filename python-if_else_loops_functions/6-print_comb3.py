108f368f1d1e495a94d5903dc3b9d2838465f2b9
zyclamenace <gemicieneshakki@gmail.com>
2024-09-04 17:18:51 +0200

    Fix : i with dizaine

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@
 # chaque combinaisons des dizaines avec chaque unité (différent de la dizaine)
 for dizaine in range(10):
     for unité in range(dizaine + 1, 10):
-        print("{:d}{:d}".format(dizaine, unité), end=", " if i < 8 else "\n")
+        print("{:d}{:d}".format(dizaine, unité), end=", " if dizaine < 8 else "\n")
