8adab5722f273683a68d150d944f915a8bce2acc
zyclamenace <gemicieneshakki@gmail.com>
2024-09-04 17:25:27 +0200

    Update : finished 9,10 and 11

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Function ADD
+
+    Args:
+        a: num1
+        b: num2
+
+    Returns:
+        Result of num1 + num2
+    """
+    return (a + b)
---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pow(a: float, b: float) -> float:
+    """
+    fonction exposant
+
+    Args:
+        a: num1
+        b: exposant
+
+    Returns:
+    Resultat de a ^ b
+    """
+    return (a ** b)
