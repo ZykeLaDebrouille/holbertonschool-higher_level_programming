564735d024e27ed453f2c8cac0df18acd0c7927c
zyclamenace <gemicieneshakki@gmail.com>
2024-09-05 09:25:18 +0200

    fix pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# chaque combinaisons des dizaines avec chaque unité (différent de la dizaine)
-for dizaine in range(10):
-    for unité in range(dizaine + 1, 10):
-        print("{:d}{:d}".format(dizaine, unité), end=", " if dizaine < 8 else "\n")
+# chaque combinaisons des dixNs avec chaque unité (différent de la dixN)
+for dixN in range(10):
+    for unité in range(dixN + 1, 10):
+        print("{:d}{:d}".format(dixN, unité), end=", " if dixN < 8 else "\n")
