641c3aeff85c607211d792ef7e856bb1e0d21b1e
zyclamenace <gemicieneshakki@gmail.com>
2024-09-09 14:01:10 +0200

    Update : task 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > my_list:
+        return None
+    elif idx == 0:
+        my_list[idx]
+    else:
+        my_list[idx] - 1
