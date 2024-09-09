7223844dbeba57745e370e0e26b71195e8472b4c
zyclamenace <gemicieneshakki@gmail.com>
2024-09-09 18:41:02 +0200

    Update : Finish task to 7 need to practice again in list and tuples

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+def no_c(my_string):
+    replaced_string = my_string.replace("c", "")
+    replaced_string = my_string.replace("C", "")
+    return replaced_string
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j, num in enumerate(i):
+            print("{:d}".format(num), end="")
+            if j != len(i) - 1:
+                print(" ", end="")
+        print()
