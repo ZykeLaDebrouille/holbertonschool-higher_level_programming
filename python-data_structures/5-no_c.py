635e4b74afda48245eb3f8268a755a8b6ed9dae4
zyclamenace <gemicieneshakki@gmail.com>
2024-09-09 19:05:27 +0200

    Fix :can't do .replace 2 times with a newline, it will erase the 1st one

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    replaced_string = my_string.replace("c", "")
-    replaced_string = my_string.replace("C", "")
+    replaced_string = my_string.replace("c", "").replace("C", "")
     return replaced_string
