18ea366f8c8ac2ab33cfe16ad7bc0b5b88202b12
zyclamenace <gemicieneshakki@gmail.com>
2024-09-10 16:10:21 +0200

    Update : task 9 and 10 finished work on local

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_value = my_list[0]
+    for num in my_list[1:]:  # Loop to check others numbers
+        if num > max_value:
+            max_value = num  # replace max_value with the higher num
+    return max_value
