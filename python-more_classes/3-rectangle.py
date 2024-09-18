c81cb73b5c6885bf9ce8aef469df165811f316e3
zyclamenace <gemicieneshakki@gmail.com>
2024-09-18 15:55:55 +0200

    Update : task 3 not finished yet

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+"""Module for a class Rectangle"""
+
+
+class Rectangle:
+    """class rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height= height
+
+    @property
+    def width(self):
+        """Getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method"""
+        if not isinstance(value ,int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method"""
+        if not isinstance(value ,int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calcul and return area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calcul perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def print(self):
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        else:
+            for width in range(self.__width):
+                print("#")
+                for height in range(self.__height):
+                    print("#")
+
