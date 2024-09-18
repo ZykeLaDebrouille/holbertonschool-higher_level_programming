0fc792c2f007469e538482e63e9bfc1f737d461e
zyclamenace <gemicieneshakki@gmail.com>
2024-09-18 13:33:05 +0200

    Update : task 1 to 2 work locally

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Module for a class Rectangle"""
+
+
+class Rectangle:
+    """class rectangle"""
+    pass
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""Module for a class Rectangle"""
+
+
+class Rectangle:
+    """class rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height= height
+
+    @property
+    def width(self):
+        """Getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method"""
+        if not isinstance(value ,int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method"""
+        if not isinstance(value ,int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calcul and return area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calcul perimeter"""
+        return 2 * (self.width + self.height)
