ca1a3e488dc46e3a265249cc00452a997518cb1a
zyclamenace <gemicieneshakki@gmail.com>
2024-09-25 15:02:31 +0200

    Test

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,11 +7,26 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    A class that represents a square, inheriting from Rectangle.
+    """
+
     def __init__(self, size):
-        self.integer_validator('size', size)
+        """
+        Initialize a new square instance.
+
+        Args:
+            size (int): The size of the square (both width and height).
+        """
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Method that return area"""
-        return self.__size ** 2
+        """
+        Returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
