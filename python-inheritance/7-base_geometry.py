f60438385609c0ce67bcccb6957a74356c3a79f0
zyclamenace <gemicieneshakki@gmail.com>
2024-09-25 14:56:22 +0200

    Fix : condition type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Fonction to pass condition"""
-        if not isinstance(value, int) or type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
