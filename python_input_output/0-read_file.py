40a9efcb6f86cc8c38fcd03261256f4f68c397b3
zyclamenace <gemicieneshakki@gmail.com>
2024-10-01 15:07:01 +0200

    Fix: no newline after printed

--- python_input_output/0-read_file.py
+++ python_input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
         filename"".
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
     # We can check that the file has been automatically closed.
     # print(f.closed) -> True
