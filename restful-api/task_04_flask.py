48fd43d42dfba7c4eacf41a747934c4787e374f9
zyclamenace <gemicieneshakki@gmail.com>
2024-10-15 15:57:22 +0200

    Update task 4 and task 5 done

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -8,20 +9,24 @@ users = {
     "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
 }
 
+
 # Endpoint racine
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
 
 # Endpoint pour obtenir la liste des utilisateurs
 @app.route('/data', methods=['GET'])
 def get_users():
     return jsonify(list(users.keys()))  # Renvoie juste les usernames
 
+
 # Endpoint pour vérifier le statut de l'API
 @app.route('/status', methods=['GET'])
 def status():
     return "OK"
+
 
 # Endpoint pour obtenir un utilisateur par son username
 @app.route('/users/<username>', methods=['GET'])
@@ -32,20 +37,22 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
+
 # Endpoint pour ajouter un utilisateur (POST)
 @app.route('/add_user', methods=['POST'])
 def add_user():
     user_data = request.get_json()
     username = user_data.get("username")
-    
+
     if not username:
         return jsonify({"error": "Username is required"}), 400
-    
+
     users[username] = user_data
     return jsonify({
         "message": "User added",
         "user": user_data
     }), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
