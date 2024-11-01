392e6738d615e7e3c73d9afe3e00c6ecfa8dcd07
zyclamenace <gemicieneshakki@gmail.com>
2024-11-01 18:34:55 +0100

    Fix: task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- Create MySQL server user user_0d_1 with all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1' @localhost IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorder tous les privilèges sur le serveur MySQL à l'utilisateur
+GRANT ALL PRIVILEGES on *.* TO  'user_0d_1'@'localhost';
+
+-- Appliquer les changements
 FLUSH PRIVILEGES;
