5ad09722be9cf7b227bb985549ea85672b75c077
zyclamenace <gemicieneshakki@gmail.com>
2024-11-01 18:16:00 +0100

    Fix task 11 shows by show and added all other files

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_shows_title ASC, tv_shows_genres.genre_id ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,6 @@
--- SQL script for task 13-count_shows_by_genre
+-- Lists all genres and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
